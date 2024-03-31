136f79a3b665062ebad835cd046edd87954a0a85
Blitzz13 <thebeardman123@gmail.com>
2024-03-31 18:58:44 +0300

    add main menu animation add cacodemon enemy improve end game scene move load game assets to utils

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,3 +1,4 @@
+import { AssetsManifest, Assets } from "pixi.js";
 import { EnemyType } from "./enums/EnemyType";
 
 export function saveScore(key: string,score: Map<EnemyType, number>) {
@@ -15,3 +16,72 @@ export function retrieveScore(key: string): Map<EnemyType, number> | null {
 
     return null;
 }
+
+export async function loadGameAssets(): Promise<void> {
+    const manifest: AssetsManifest = {
+      bundles: [
+        {
+          name: "environment",
+          assets: [
+            {
+              name: "corridor",
+              srcs: "./assets/map2.png",
+            },
+            {
+              name: "background_hell",
+              srcs: "./assets/backgorund_hell.json",
+            },
+            {
+              name: "main_menu_anim",
+              srcs: "./assets/main_menu_anim.json",
+            },
+          ],
+        },
+        {
+          name: "characters",
+          assets: [
+            {
+              name: "doomguy",
+              srcs: "./assets/doomguy_walking.json",
+            },
+            {
+              name: "doomguy_death",
+              srcs: "./assets/doomguy_death.json",
+            },
+            {
+              name: "grunt_idle",
+              srcs: "./assets/grunt_idle.json",
+            },
+            {
+              name: "grunt_death",
+              srcs: "./assets/grunt_death.json",
+            },
+            {
+              name: "caocdemon",
+              srcs: "./assets/cacodemon.json",
+            },
+          ],
+        },
+        {
+          name: "objects",
+          assets: [
+            {
+              name: "bullet",
+              srcs: "./assets/bullet.png",
+            },
+            {
+              name: "muzzle",
+              srcs: "./assets/muzzle_flash.json",
+            },
+            {
+              name: "green_ball",
+              srcs: "./assets/green_ball.png",
+            },
+          ],
+        },
+      ],
+    };
+  
+    await Assets.init({ manifest });
+    await Assets.loadBundle(["environment", "characters", "objects"]);
+  }
--- src/configs/EnemyConfigs.ts
+++ src/configs/EnemyConfigs.ts
@@ -4,6 +4,7 @@ import { DeathType } from "../enums/DeathType";
 import { EnemyType } from "../enums/EnemyType";
 
 export const gruntConfig: IEnemyConfig = {
+    health: 2,
     type: EnemyType.GRUNT,
     rateOfFire: 100,
     damage: 1,
@@ -19,6 +20,7 @@ export const gruntConfig: IEnemyConfig = {
         texture: "bullet",
         anchor: new Point(0.5),
         scale: new Point(0.1, 0.1),
+        startPosOffset: new Point(),
     },
     death: {
         types: new Map([
@@ -28,6 +30,40 @@ export const gruntConfig: IEnemyConfig = {
                 "grunt_death_3.png",
                 "grunt_death_4.png",
                 "grunt_death_5.png",
+            ]],
+        ]),
+        animationSpeed: 0.2,
+        loop: false,
+    }
+}
+
+export const cacodemonConfig: IEnemyConfig = {
+    health: 4,
+    type: EnemyType.CACODEMON,
+    rateOfFire: 300,
+    damage: 3,
+    meleeDamage: 4,
+    speed: 0.85,
+    characterTextures: {
+        left: "cacodemon_idle_left.png",
+        down: "cacodemon_idle_down.png",
+        up: "cacodemon_idle_up.png",
+    },
+    projectile: {
+        speed: 5,
+        texture: "green_ball",
+        anchor: new Point(0.5),
+        scale: new Point(0.1, 0.1),
+        startPosOffset: new Point(0, 30),
+    },
+    death: {
+        types: new Map([
+            [DeathType.SHOT, [
+                "cacodemin_death_1.png",
+                "cacodemin_death_2.png",
+                "cacodemin_death_3.png",
+                "cacodemin_death_4.png",
+                "cacodemin_death_5.png",
             ]],
         ]),
         animationSpeed: 0.2,
