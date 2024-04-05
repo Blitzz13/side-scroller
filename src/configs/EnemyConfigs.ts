282850dcd4d034201f08dbddc2a2399034338d33
Blitzz13 <thebeardman123@gmail.com>
2024-04-06 01:15:46 +0300

    Add at at as an enemy

--- src/configs/EnemyConfigs.ts
+++ src/configs/EnemyConfigs.ts
@@ -4,11 +4,11 @@ import { EnemyType } from "../enums/EnemyType";
 
 const atStConfig: IEnemyConfig = {
     faceTarget: true,
-    health: 1,
+    health: 2,
     type: EnemyType.AT_ST,
     rateOfFire: 300,
     damage: 1,
-    meleeDamage: 1,
+    meleeDamage: 2,
     speed: 2.16,
     characterTextures: {
         left: "at_st_left.png",
@@ -39,6 +39,50 @@ const atStConfig: IEnemyConfig = {
     },
 }
 
+const atAtConfig: IEnemyConfig = {
+    faceTarget: true,
+    health: 8,
+    type: EnemyType.AT_AT,
+    rateOfFire: 500,
+    damage: 4,
+    meleeDamage: 6,
+    speed: 2.16,
+    characterTextures: {
+        left: "at_at_left.png",
+        down: "at_at_down.png",
+        up: "at_at_up.png",
+    },
+    projectile: {
+        speed: 12,
+        texture: "laser",
+        anchor: new Point(0.5),
+        scale: new Point(0.4, 0.4),
+        startPosOffset: new Point(-10, 8),
+    },
+    death: {
+        animation: [
+            "at_at_death_1.png",
+            "at_at_death_2.png",
+            "at_at_death_3.png",
+            "at_at_death_4.png",
+            "at_at_death_5.png",
+            "at_at_death_6.png",
+            "at_at_death_7.png",
+            "at_at_death_8.png",
+            "at_at_death_9.png",
+            "at_at_death_10.png",
+            "at_at_death_11.png",
+            "at_at_death_12.png",
+        ],
+        animationSpeed: 0.1,
+        loop: false,
+    },
+    spawnRange: {
+        min: new Point(1280, 610),
+        max: new Point(2000, 610),
+    },
+}
+
 const viperDroidConfig: IEnemyConfig = {
     health: 1,
     type: EnemyType.VIPER_DROID,
@@ -61,4 +105,5 @@ const viperDroidConfig: IEnemyConfig = {
 export const enemyConfigs: IEnemyConfig[] = [
     atStConfig,
     viperDroidConfig,
+    atAtConfig
 ]
